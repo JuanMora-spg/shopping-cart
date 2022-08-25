88cb530d28ecbb482a991683d15e1f0407225eab
JuanMora-spg <juanmora.lpz18@gmail.com>
2022-08-25 15:08:02 -0500

    refactor: remove local state in NumberInput component

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,38 +1,20 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../styles/NumberInput.scss";
 
-function NumberInput() {
-  const [value, setValue] = useState(0);
-
-  const handleIncrease = () => setValue((prevValue) => prevValue + 1);
-
-  const handleDecrease = () =>
-    setValue((prevValue) => (prevValue > 0 ? prevValue - 1 : prevValue));
-
+function NumberInput({ num }) {
   return (
     <div className="number-input">
-      <button
-        type="button"
-        title="remove"
-        className="number-input__btn"
-        onClick={handleDecrease}
-      >
+      <button type="button" title="remove" className="number-input__btn">
         <FontAwesomeIcon icon={faMinus} />
       </button>
       <input
-        value={value}
+        value={num}
         type="number"
         className="number-input__input"
         readOnly
       />
-      <button
-        type="button"
-        title="add"
-        className="number-input__btn"
-        onClick={handleIncrease}
-      >
+      <button type="button" title="add" className="number-input__btn">
         <FontAwesomeIcon icon={faPlus} />
       </button>
     </div>
