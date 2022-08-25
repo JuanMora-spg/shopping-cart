aa70d86c1e1c4b7fcbb1635cf7181a93e1c6ce41
JuanMora-spg <juanmora.lpz18@gmail.com>
2022-08-24 23:50:55 -0500

    feat: connect ProductDetails component with redux store

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,24 +1,32 @@
+import { useSelector } from "react-redux";
+import { useParams, Navigate } from "react-router-dom";
 import { NumberInput } from "../components/NumberInput";
+import { NotFound } from "../pages";
 import "../styles/ProductDetails.scss";
 
 function ProductDetails() {
+  const { id } = useParams();
+
+  //Checking wether with this id exists
+  const { products } = useSelector((state) => state.shop);
+
+  const selectedProduct = products.find((product) => product.id === Number(id));
+
+  if (!selectedProduct) {
+    return <NotFound />;
+  }
+
+  const { image, title, description, price } = selectedProduct;
+
   return (
     <article className="product-details">
       <picture className="product-details__img-container">
-        <img
-          className="product-details__img"
-          src="https://images.unsplash.com/photo-1509913841876-b8a297b4e240?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          alt="product image"
-        />
+        <img className="product-details__img" src={image} alt="product image" />
       </picture>
       <div className="product-details__content">
-        <h2 className="product-details__title">Clothe name</h2>
-        <p className="product-details__description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus a
-          aut eaque neque fugiat asperiores, repellendus facere ut nobis? Ad eum
-          officia ex impedit eius alias praesentium quasi itaque sunt!
-        </p>
-        <p className="product-details__price">$ price</p>
+        <h2 className="product-details__title">{title}</h2>
+        <p className="product-details__description">{description}</p>
+        <p className="product-details__price">$ {price}</p>
         <div className="product-details__actions">
           <button className="product-details__btn">Add to Cart</button>
           <div className="product-details__number-input">
