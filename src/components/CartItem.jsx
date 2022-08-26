1c8b201eafb02f34564c1c854fd19865eb6289a6
JuanMora-spg <juanmora.lpz18@gmail.com>
2022-08-25 23:00:20 -0500

    feat: connect CartItem component with redux store

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,25 +1,44 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart, deleteFromCart } from "../store/cart/cart.actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { NumberInput } from "./";
 import "../styles/CartItem.scss";
 
-function CartItem() {
+function CartItem({ item }) {
+  const { id, title, image, price, quantity } = item;
+  const dispatch = useDispatch();
+
+  const calculatePrice = () => {
+    return price * quantity;
+  };
+
+  const handleRemoveFromCart = () => dispatch(deleteFromCart(id));
+  const handleAddToCart = () => dispatch(addToCart(item));
+  const handleRemoveAllItemsFromCart = () => dispatch(deleteFromCart(id, true));
+
   return (
     <div className="cart-item">
-      <button type="button" title="remove" className="cart-item__delete">
+      <button
+        type="button"
+        title="remove"
+        className="cart-item__delete"
+        onClick={handleRemoveAllItemsFromCart}
+      >
         <FontAwesomeIcon icon={faTrash} />
       </button>
-      <h4 className="cart-item__title">Article name</h4>
-      <img
-        className="cart-item__img"
-        src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="item photo"
-      />
+      <h4 className="cart-item__title">{title}</h4>
+      <img className="cart-item__img" src={image} alt="item photo" />
       <p className="cart-item__text cart-item__text--center">
-        $900 x 2 = <span className="cart-item__text--bold">$18,000</span>
+        {price} x {quantity} =
+        <span className="cart-item__text--bold"> $ {calculatePrice()}</span>
       </p>
       <div className="cart-item__number-input">
-        <NumberInput />
+        <NumberInput
+          num={quantity}
+          handleMinusBtn={handleRemoveFromCart}
+          handlePlusBtn={handleAddToCart}
+        />
       </div>
     </div>
   );
