b86f9b4422c7d71fe31b63d1e8e382c808f63dde
JuanMora-spg <juanmora.lpz18@gmail.com>
2022-08-25 19:53:11 -0500

    feat: implement function to get total items in cart

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { useModal } from "../hooks/useModal";
@@ -8,6 +9,12 @@ import "../styles/CartIcon.scss";
 function CartIcon() {
   const { isOpen: isOpenCartModal, handleOpen, handleClose } = useModal(false);
 
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const totalItemsInCart = cartItems.reduce((acc, current) => {
+    return acc + current.quantity;
+  }, 0);
+
   return (
     <>
       <div className="cart-icon" onClick={() => handleOpen()}>
@@ -15,7 +22,7 @@ function CartIcon() {
         <button type="button" title="shopping cart" className="cart-icon__btn">
           <FontAwesomeIcon icon={faCartShopping} />
         </button>
-        <span className="cart-icon__notification">3</span>
+        <span className="cart-icon__notification">{totalItemsInCart}</span>
       </div>
       <Modal isOpen={isOpenCartModal}>
         <CartDetail handleClose={handleClose} />
